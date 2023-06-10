c1f36e1dc12e43c88549fe491b7d79e19c1a0bad
JadenYee <xxxJadenYee@gmail.com>
2023-06-10 14:16:40 +0800

    new module

---
+++ FactoryMode/src/main/java/personal/jadenyee/App.java
@@ -0,0 +1,13 @@
+package personal.jadenyee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
